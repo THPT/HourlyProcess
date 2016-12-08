28d8783af94049164070228328a7eeaee5a9a9bd
triet <triet.phm@gmail.com>
2016-12-09 01:14:42 +0700

    Fix wrong span time

---
+++ README.md
@@ -0,0 +1,5 @@
+Hourly process Daily active user
+
+```
+mvn clean && mvn compile && mvn package && spark-submit --class DailyProcess target/DailyProcess-0.0.1.jar
+```
--- src/process/Process.java
+++ src/process/Process.java
@@ -35,7 +35,7 @@ public class Process {
 
 	public static void exportHourlyUser(SparkSession spark) {
 		long currentDate = System.currentTimeMillis() / 1000;
-		long hourBefore = System.currentTimeMillis() / 1000 - 72 * 3600;
+		long hourBefore = System.currentTimeMillis() / 1000 - 3600;
 		String query = "SELECT DISTINCT uuid FROM events WHERE created_at >= " + hourBefore + " AND created_at <= "
 				+ currentDate;
 		System.out.println(query);
@@ -62,48 +62,11 @@ public class Process {
 			sb.append("('").append(n).append("','").append(date).append("')");
 			
 		}
-//		List<String> listVal = rddStr.collect();
-//
-//		for (int i = 0; i < listVal.size(); i++) {
-//			listVal.set(i, "('" + listVal.get(i) + "','" + date + "')");
-//		}
-//
-//		StringBuilder sb = new StringBuilder();
-//		for (String n : listVal) {
-//			if (sb.length() > 0)
-//				sb.append(',');
-//			sb.append("('").append(n).append("','").append(date).append("')");
-//		}
-		String values = sb.toString();
 
-		// String values = rddStr.reduce(new Function2<String, String, String>()
-		// {
-		//
-		// @Override
-		// public String call(String v1, String v2) throws Exception {
-		// System.out.println("v1: '" + v1 + "'");
-		// System.out.println("v2: '" + v2 + "'");
-		// String res = v1 + "('" + v2 + "','" + date + "'),";
-		// System.out.println("res: " + res);
-		// return res;
-		// }
-		// });
-		// String values = rddStr.fold("|", new Function2<String, String,
-		// String>() {
-		//
-		// @Override
-		// public String call(String v1, String v2) throws Exception {
-		// if (v2.trim().equals("") || v2.equals("|")) {
-		// return v1;
-		// }
-		// System.out.println("v1: '" + v1 + "'");
-		// System.out.println("v2: '" + v2 + "'");
-		// String res = v1 + "('" + v2 + "','" + date + "'),";
-		// System.out.println("res: " + res);
-		// return res;
-		// }
-		// });
-		// values = values.substring(0, values.length() - 2);
+		String values = sb.toString();
+		if (values.length() == 0){
+			return;
+		}
 		try {
 			connection.createStatement();
 			String q = String.format("INSERT INTO user_actives(uuid, created_at) VALUES %s", values);
@@ -116,6 +79,8 @@ public class Process {
 		}
 	}
 
+	
+	
 	public static void main(String[] args) {
 		try {
 			connection = createConnection();
@@ -126,6 +91,7 @@ public class Process {
 
 		SparkSession spark = SparkSession.builder().appName("ProcessingData")
 				.config("spark.sql.warehouse.dir", "/user/hive/warehouse").enableHiveSupport().getOrCreate();
+		
 		spark.sql("show tables").show();
 
 		exportHourlyUser(spark);
